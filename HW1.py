d78dd1a5da3275673b507c377d259883269803b2
ta-will <xxdrtevxx@gmail.com>
2016-08-25 15:14:51 -0500

    Update

--- HW1.py
+++ HW1.py
@@ -30,6 +30,50 @@ for i in range(0, 6, 1):
 	_txtfile.write(str(partA[i]));
 	_txtfile.write('\n');
 
+_txtfile.write('\n');
 #outputPart(partA);
 
+#Part B
+L = [2, 3, 5, 7, 11, 9, 13, 23, 29, -1];
+
+a = L[-3];
+b = L[3:7];
+c = L.append(37);
+d = L.insert(7, 29);
+e = L.sort();
+f = L.reverse();
+g = len(L);
+
+M = [a, b, c, d, e, f, g];
+
+i = 0;
+
+for i in range(0, 7, 1):
+	_txtfile.write(str(M[i]));
+	_txtfile.write('\n');
+
+_txtfile.write('\n');
+
+#Part 3
+t = (1, 2, 3, 4, 5, 6);
+
+a = t[3];
+b = t[-3];
+c = t = 4, 8, 12, 16;
+d = len(t);
+
+M = [a, b, c, d]
+
+i = 0;
+
+for i in range(0, 4, 1):
+	_txtfile.write(str(M[i]));
+	_txtfile.write('\n');
+
+#Part 4
+D = {"Fortran":1956, "Cobol":1960, "Lisp":1960, "C":1972, "Pascal":1972, "Python":1991};
+
+a = D["Cobol"];
+b = D["Python"] -= 2;
+c = len(D);
 _txtfile.close();
